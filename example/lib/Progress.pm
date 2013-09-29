a57d3f7b9ab6fd6623cd95ca2fc8ff16811d6cb2
Steven Humphrey <catchgithub@33k.co.uk>
2013-09-29 17:38:05 +0100

    Improved example, added a bootstrap demo

--- example/lib/Progress.pm
+++ example/lib/Progress.pm
@@ -1,3 +1,10 @@
+=head1 DESCRIPTION
+
+A sample dancer app that demonstrates a simple progress bar.
+Both a text update and a bootstrap based progress bar
+
+=cut
+
 package Progress;
 use strict;
 use warnings;
@@ -5,14 +12,66 @@ use warnings;
 use Dancer2;
 use Dancer2::Plugin::ProgressStatus;
 
-get '/test' => sub {
-    if ( is_progress_running('test') ) {
+# Landing demo page.
+# Shows a link and some progress text
+get '/' => sub {
+    return <<'EOF';
+<html>
+<head>
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script src="/client_progress.js"></script>
+<script type="text/javascript">
+    $(function() {
+        $('#longrunninglink').click(function() {
+            // Issue the long running task in the background for demo purposes
+            $.getJSON('/start_long_task/test');
+
+            // wait a second to ensure the first call arrives
+            // first, then start monitoring the progress
+            setTimeout(function() {
+                checkProgress('test', function(data) {
+                var progress = Math.round((data.count / data.total ) * 100);
+                    $('#progress').html(progress + '%');
+                    $('.progress-bar').width(progress + '%');
+                });
+            }, 1000);
+        });
+    });
+</script>
+</head>
+<body>
+<h1>Demo for progress bar</h1>
+
+<div>
+<a id="longrunninglink" href="#">
+Click here
+</a> to start a long running server task in the background.
+</div>
+
+<!-- plain text % status -->
+<div id="progress"></div>
+
+<!-- this is a bootstrap progressbar, requires bootstrap css loaded  -->
+<div id="progressbar" class="progress progress-striped active">
+    <div class="progress-bar" style="width: 1%;"></div>
+</div>
+
+</body>
+</html>
+EOF
+};
+
+## This loads the long running query and takes about 30 seconds before
+## it returns a single string "ok"
+get '/start_long_task/:name' => sub {
+    if ( is_progress_running(param('name')) ) {
         return 'Progress "test" is already running, please wait until it finishes';
     }
 
-    my $prog = start_progress_status('test');
+    my $prog = start_progress_status(param('name'));
 
-    foreach my $i (1..10) {
+    foreach my $i (1..30) {
         $prog++;
         $prog->add_message("finished $i");
         sleep 1;
@@ -21,43 +80,6 @@ get '/test' => sub {
 
     content_type 'text/plain';
     return "ok";
-};
-
-get '/testres' => sub {
-    content_type 'text/html';
-
-    return <<'EOF';
-<html>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-<script type="text/javascript">
-    function displayProgress(data, done) {
-        var prog = (data.count / data.total) * 100;
-        $('#progress').html(Math.round(prog) + '%');
-        if ( done ) {
-            $('#progress').append("<br />Done!");
-        }
-    }
-    function checkProgress() {
-        $.getJSON('/_progress_status/test', function(data) {
-            if ( !data.in_progress ) {
-                displayProgress(data, true);
-                return;
-            }
-            displayProgress(data);
-            setTimeout(checkProgress, 1000);
-        })
-    }
-
-    checkProgress();
-</script>
-<body>
-Test Progress
-<div id="progress">
-0%
-</div>
-</body>
-</html>
-EOF
 };
 
 1;
